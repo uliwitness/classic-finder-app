14a6a072087d09fcd4d99256360b44ab4ff42ced
Ben Szymanski <bszyman@protypesoftware.com>
2017-10-21 16:04:19 -0400

    File system operations progress checkin.

--- Classic Finder/CFRFileSystemOperations.m
+++ Classic Finder/CFRFileSystemOperations.m
@@ -42,9 +42,25 @@
 }
 
 + (void)createNewFolderInDirectory:(NSURL *)directory
-                             named:(NSString *)folderName
+                             named:(NSString *)directoryName
 {
+    NSURL *parentDirectoryOfOriginalDirectory = [directory URLByDeletingLastPathComponent];
     
+    NSString *pathOfNewDirectory = [NSString stringWithFormat:@"%@/%@", parentDirectoryOfOriginalDirectory.absoluteString, directoryName];
+    
+    // Create and run copy/duplicate task
+    NSTask *createDirectoryTask = [[NSTask alloc] init];
+    NSArray *args = @[pathOfNewDirectory];
+    
+    [createDirectoryTask setLaunchPath:@"/bin/mkdir"];
+    [createDirectoryTask setArguments:args];
+    
+    @try {
+        [createDirectoryTask launch];
+    }
+    @catch (NSException *e) {
+        NSLog(@"Error while running create directory operation. (%@ / %@)", e.name, e.reason);
+    }
 }
 
 + (void)printFile:(NSURL *)file
@@ -56,7 +72,7 @@
 {
     // Create file name for new file
     NSString *fileName = [file lastPathComponent];
-    NSString *regexPattern = @"(.+)\\.(.+)";
+    NSString *regexPattern = @"([A-Za-z0-9_ ]+)+";
     
     NSError *regexError = nil;
     NSRegularExpression *fileNameRegex = [[NSRegularExpression alloc] initWithPattern:regexPattern
@@ -128,13 +144,161 @@
 }
 
 + (void)renameFile:(NSURL *)file
+                to:(NSString *)newName
 {
+    NSString *originalFileName = [file lastPathComponent];
+    NSString *regexPattern = @"([A-Za-z0-9_ ]+)+";
     
+    NSError *regexError = nil;
+    NSRegularExpression *fileNameRegex = [[NSRegularExpression alloc] initWithPattern:regexPattern
+                                                                              options:NSRegularExpressionCaseInsensitive
+                                                                                error:&regexError];
+    
+    if (regexError != nil) {
+        NSLog(@"Error while parsing filename. (%@)", regexError.description);
+    } else {
+        NSArray *fileNameCaptureGroups = [fileNameRegex matchesInString:originalFileName
+                                                                options:0 range:NSMakeRange(0, [originalFileName length])];
+        if ([fileNameCaptureGroups count] >= 2) {
+            NSString *newFileName = [NSString stringWithFormat:@"%@", newName];
+            
+            // start at the second array index
+            for (NSUInteger x = 1; x < [fileNameCaptureGroups count]; x += 1) {
+                NSString *fileNameExtra = [fileNameCaptureGroups objectAtIndex: x];
+                newFileName = [NSString stringWithFormat:@"%@.%@", newFileName, fileNameExtra];
+            }
+            
+            NSString *pathOfOriginalFile = [file absoluteString];
+            NSURL *parentDirectoryOfOriginalFile = [file URLByDeletingLastPathComponent];
+            
+            NSString *pathOfNewFile = [NSString stringWithFormat:@"%@/%@", parentDirectoryOfOriginalFile.absoluteString, newFileName];
+            
+            // Create and run copy/duplicate task
+            NSTask *moveFileTask = [[NSTask alloc] init];
+            NSArray *args = @[pathOfOriginalFile, pathOfNewFile];
+            
+            [moveFileTask setLaunchPath:@"/bin/mv"];
+            [moveFileTask setArguments:args];
+            
+            @try {
+                [moveFileTask launch];
+            }
+            @catch (NSException *e) {
+                NSLog(@"Error while running move file operation. (%@ / %@)", e.name, e.reason);
+            }
+        } else {
+            NSLog(@"Error while creating new file name - not parsed correctly or missing extension.");
+        }
+    }
 }
 
 + (void)renameDirectory:(NSURL *)directory
+                     to:(NSString *)newName
 {
+    NSString *pathOfOriginalDirectory = [directory absoluteString];
+    NSURL *parentDirectoryOfOriginalDirectory = [directory URLByDeletingLastPathComponent];
     
+    NSString *pathOfNewDirectory = [NSString stringWithFormat:@"%@/%@", parentDirectoryOfOriginalDirectory.absoluteString, newName];
+    
+    // Create and run copy/duplicate task
+    NSTask *renameTask = [[NSTask alloc] init];
+    NSArray *args = @[pathOfOriginalDirectory, pathOfNewDirectory];
+    
+    [renameTask setLaunchPath:@"/bin/mv"];
+    [renameTask setArguments:args];
+    
+    @try {
+        [renameTask launch];
+    }
+    @catch (NSException *e) {
+        NSLog(@"Error while running rename directory operation. (%@ / %@)", e.name, e.reason);
+    }
+}
+
++ (void)moveFileToTrash:(NSURL *)file
+{
+    NSString *pathOfFile = [file absoluteString];
+    
+    NSURL *userTrashDirectory = [NSURL URLWithString:@"~/.Tash"];
+    NSString *userTrashDirectoryFullPath = [userTrashDirectory absoluteString];
+    
+    // Create and run copy/duplicate task
+    NSTask *trashFileTask = [[NSTask alloc] init];
+    NSArray *args = @[pathOfFile, userTrashDirectoryFullPath];
+    
+    [trashFileTask setLaunchPath:@"/bin/mv"];
+    [trashFileTask setArguments:args];
+    
+    @try {
+        [trashFileTask launch];
+    }
+    @catch (NSException *e) {
+        NSLog(@"Error while running delete file operation. (%@ / %@)", e.name, e.reason);
+    }
+}
+
++ (void)moveDirectoryToTrash:(NSURL *)directory
+{
+    NSString *pathOfDirectory = [directory absoluteString];
+    
+    NSURL *userTrashDirectory = [NSURL URLWithString:@"~/.Tash"];
+    NSString *userTrashDirectoryFullPath = [userTrashDirectory absoluteString];
+    
+    // Create and run copy/duplicate task
+    NSTask *trashDirectoryTask = [[NSTask alloc] init];
+    NSArray *args = @[pathOfDirectory, userTrashDirectoryFullPath];
+    
+    [trashDirectoryTask setLaunchPath:@"/bin/mv"];
+    [trashDirectoryTask setArguments:args];
+    
+    @try {
+        [trashDirectoryTask launch];
+    }
+    @catch (NSException *e) {
+        NSLog(@"Error while running delete directory operation. (%@ / %@)", e.name, e.reason);
+    }
+}
+
++ (void)moveFile:(NSURL *)file
+   toNewLocation:(NSURL *)location
+{
+    NSString *pathOfFile = [file absoluteString];
+    NSString *pathOfNewLocation = [location absoluteString];
+    
+    // Create and run copy/duplicate task
+    NSTask *moveFileTask = [[NSTask alloc] init];
+    NSArray *args = @[pathOfFile, pathOfNewLocation];
+    
+    [moveFileTask setLaunchPath:@"/bin/mv"];
+    [moveFileTask setArguments:args];
+    
+    @try {
+        [moveFileTask launch];
+    }
+    @catch (NSException *e) {
+        NSLog(@"Error while running move file operation. (%@ / %@)", e.name, e.reason);
+    }
+}
+
++ (void)moveDirectoryToTrash:(NSURL *)directory
+               toNewLocation:(NSURL *)location
+{
+    NSString *pathOfDirectory = [directory absoluteString];
+    NSString *pathOfNewLocation = [location absoluteString];
+    
+    // Create and run copy/duplicate task
+    NSTask *moveDirectoryTask = [[NSTask alloc] init];
+    NSArray *args = @[pathOfDirectory, pathOfNewLocation];
+    
+    [moveDirectoryTask setLaunchPath:@"/bin/mv"];
+    [moveDirectoryTask setArguments:args];
+    
+    @try {
+        [moveDirectoryTask launch];
+    }
+    @catch (NSException *e) {
+        NSLog(@"Error while running move directory operation. (%@ / %@)", e.name, e.reason);
+    }
 }
 
 + (void)createSymLinkOfFile:(NSURL *)file
