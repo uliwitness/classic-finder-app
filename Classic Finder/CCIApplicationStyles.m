eeb75261277d694f766eb7a080f46cbf0ffce271
Ben Szymanski <bszyman@protypesoftware.com>
2017-12-03 01:23:07 -0500

    Create Application Styles singleton class.

---
+++ Classic Finder/CCIApplicationStyles.m
@@ -0,0 +1,87 @@
+//
+//  CCIApplicationStyles.m
+//  Classic Finder
+//
+//  Created by Ben Szymanski on 12/2/17.
+//  Copyright © 2017 Protype Software Ltd. All rights reserved.
+//
+//
+// This file is part of Classic Finder.
+//
+// Classic Finder is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Classic Finder is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Classic Finder.  If not, see <http://www.gnu.org/licenses/>.
+
+#import "CCIApplicationStyles.h"
+
+@interface CCIApplicationStyles()
+{
+    NSColor *darkPurpleColor;
+}
+
+//@property (strong) NSColor *darkPurpleColor;
+
+@end
+
+@implementation CCIApplicationStyles
+
+#pragma mark - INITIALIZATION
+
++ (instancetype)instance
+{
+    static CCIApplicationStyles *styleStore = nil;
+    static dispatch_once_t initOneTimeToken;
+    
+    dispatch_once(&initOneTimeToken, ^{
+        styleStore = [[self alloc] initHidden];
+    });
+    
+    return styleStore;
+}
+
+- (instancetype)init
+{
+    @throw [NSException exceptionWithName:@"Singleton Init Warning"
+                                   reason:@"Use +[instance]"
+                                 userInfo:nil];
+    
+    return nil;
+}
+
+- (instancetype)initHidden
+{
+    self = [super init];
+    
+    if (self) {
+
+    }
+    
+    return self;
+}
+
+#pragma mark - COLORS
+
+- (NSColor *)darkPurpleColor
+{
+    if (darkPurpleColor == nil) {
+        darkPurpleColor = [NSColor colorWithCalibratedRed:0.15
+                                                    green:0.14
+                                                     blue:0.31
+                                                    alpha:1.0];
+    }
+    
+    return darkPurpleColor;
+}
+
+
+
+@end
