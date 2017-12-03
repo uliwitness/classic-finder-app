ae1f74a46d80c1b200d070ff0fab4708455972fd
Ben Szymanski <bszyman@protypesoftware.com>
2017-12-02 22:12:47 -0500

    Apply GPLv3 License.

--- Classic Finder/CCIWindowGripButton.h
+++ Classic Finder/CCIWindowGripButton.h
@@ -5,6 +5,21 @@
 //  Created by Ben Szymanski on 11/23/17.
 //  Copyright © 2017 Ben Szymanski. All rights reserved.
 //
+//
+// This file is part of Classic Finder.
+//
+// Classic Finder is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Classic Finder is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Classic Finder.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <Cocoa/Cocoa.h>
 
